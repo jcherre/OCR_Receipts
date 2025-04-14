1d6e3d1063c03717eddaba52c9d4a291dd45e309
Renato <renato.capcha.c@gmail.com>
2025-04-13 22:54:41 -0500

    refac: refactoring del archivo utils.py

--- module/utils.py
+++ module/utils.py
@@ -4,7 +4,20 @@ import numpy as np
 
 from PIL import Image
 
-def allowed_file(filename, allowed_extensions):
+def allowed_file(filename: str, allowed_extensions:list) -> bool:
+    """
+    Checks if a file name has an allowed extension.
+    This function takes a file name and a set of allowed extensions and determines whether the file extension (if any)
+    falls within the set of allowed extensions. The extension comparison is performed case-insensitively.
+
+    :param filename:  The name of the file to be verified.
+    :type filename: str
+    :param allowed_extensions: A set of strings representing the allowed file extensions (e.g., {'pdf', 'png', 'jpg'}).
+                               It is recommended to use a set for more efficient searches.
+    :type allowed_extensions: list
+    :return bool: True if the file has an allowed extension, False otherwise. Returns False if the file name does not
+                  contain a period ('.') or if the extracted extension is not in the set of `allowed_extensions`.
+    """
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
 
 
@@ -16,6 +29,7 @@ def is_number(string: str) -> bool:
     decimal places). If the conversion fails, a `ValueError` exception is caught, indicating that the string is not in
     a recognizable numeric format.
     :param string: The text string you want to verify.
+    :type string: str
     :return bool: True if the string can be converted to a floating-point number, False otherwise.
     """
     try:
@@ -25,7 +39,7 @@ def is_number(string: str) -> bool:
         return False
 
 
-def assign_price( price, text, set_prices):
+def assign_price(price: str, text: str, set_prices: dict) -> dict:
     """
     Assigns a price value to different price categories (VAT, discounts, transactions, etc.) within a dictionary,
     based on the presence of specific keywords in the associated text.
@@ -52,7 +66,7 @@ def assign_price( price, text, set_prices):
     return set_prices
 
 
-def process_pdf(pdf_path) -> np.ndarray:
+def process_pdf(pdf_path: np.array) -> np.ndarray:
     """
     Converts the first page of a PDF file to a NumPy array of an image.
     :param pdf_path: The path to the PDF file.
